6f50c795c5f01ef08f6a8a3ae89dba29513070d2
mostafa <info@alborzvoip.ir>
2025-10-05 23:17:50 +0330

    with-encoders-pakage

--- sipphone3/obj/Debug/net8.0-windows10.0.17763/sipphone3.AssemblyInfo.cs
+++ sipphone3/obj/Debug/net8.0-windows10.0.17763/sipphone3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sipphone3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea867b2bfe35690f39648124fcc0ae4db031a2a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("sipphone3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sipphone3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
